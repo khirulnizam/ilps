e43d5a41dda99d9e402146e877312b797c910351
khirulnizam <kerul76>
2019-02-12 12:10:03 +0800

    userlevel pelajar

--- verify.php
+++ verify.php
@@ -12,15 +12,26 @@ $sql="SELECT * FROM pengguna
 //laksana dalam database
 include "connection.php";
 $rs=mysqli_query($db, $sql);
+$rekod=mysqli_fetch_array($rs);
 //namapengguna & katalaluan match
 if(mysqli_num_rows($rs)==1){
     //echo "Nama pengguna dijumpai";
+    
     //register session
     session_start();
     $_SESSION['sessionid']=session_id();
     //simpan namapengguna dalam session
     $_SESSION['namapengguna']=$namapengguna;
-    header ("Location: searchadmin.php");
+    //capture userlevel
+    $userlevel=$rekod['userlevel'];
+    $_SESSION['userlevel']=$userlevel;
+    $_SESSION['namapenuh']=$rekod['namapenuh'];
+    $_SESSION['idpelajar']=$rekod['IDPelajar'];
+    if($userlevel=="admin"){
+        header ("Location: searchadmin.php");
+    }else if ($userlevel=="pelajar"){
+        header ("Location: infopelajar.php");
+    }//end userlevel
 }else{//not match
     header ("Location: login.php?error=Nama pengguna 
     dan katalaluan takpadan");
